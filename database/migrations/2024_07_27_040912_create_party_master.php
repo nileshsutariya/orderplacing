90abc3c4d0771f26404ec57c0b7e2e91faebbdff
nileshsutariya <nilesh.netdeveloper@gmail.com>
2024-07-27 09:49:43 +0530

    git inti

---
+++ database/migrations/2024_07_27_040912_create_party_master.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('party_master', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('phone_number');
+            $table->string('gst');
+            $table->string('pancard_no');
+            $table->boolean('status');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('party_master');
+    }
+};
