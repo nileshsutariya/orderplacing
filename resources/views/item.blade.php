789b4333219cfa830a1467b53ca0570d08cd9f83
nileshsutariya <nilesh.netdeveloper@gmail.com>
2024-07-27 12:28:17 +0530

    git inti

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,3 +1,34 @@
 @include('layouts.header')
-<h3>sdfghjkl</h3>
+
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Company data</h3>
+                        <input class="form-control" type="search" id="search" name="search" placeholder="Search">
+                    <div class="card-body">
+                        <table class="table  table-hover table-valign-middle table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>company</th>
+                                    <th>email</th>
+                                    <th>address</th>
+                                    <th>phone number</th>
+                                    <th>Status</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 @include('layouts.footer')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -33,38 +33,52 @@
   <!-- summernote -->
   <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
 </head>
-
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+      </ul>
 
-  </nav>
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo --> 
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+
+
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
       <span class="brand-text font-weight-light">Order Placing</span>
     </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
- <nav>
-     
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+
+          <div class="info">
+            <a href="#" class="d-block"><b>Menu</b></a>
+          </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+        </nav>
+        <!-- /.sidebar-menu -->
       </div>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper mt-3">
   
