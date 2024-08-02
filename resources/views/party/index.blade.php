3304df54c8004bb2d1728bcb6e803ab1c9e66fff
nileshsutariya <nilesh.netdeveloper@gmail.com>
2024-08-02 15:16:29 +0530

    git inti

--- resources/views/party/index.blade.php
+++ resources/views/party/index.blade.php
@@ -4,6 +4,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+
     <title>Login </title>
 </head>
 
@@ -14,7 +20,7 @@
         <div class="row">
             <div class="col-md-12 mt-5 p-4">
                 <div class="card card-outline card-info mx-auto"  style="width: 40rem; bordered: 5px;">
-                    <div class="card-header">
+                    <div class="card-header text-">
                         <h3 class="card-title">Party</h3>
                     </div>
                     <div class="card-body">
@@ -119,6 +125,7 @@
                             </div>
                       </div>
                       </div>
+                      <br>
                         <div class="row">
                             <div class="col-sm-4">
                                 <div class="form-group clearfix">
@@ -130,7 +137,7 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div><br>  
                         <input type="hidden" class="form-control" id="id" name="id"
                             value="{{isset($party)? $party->id:''}} ">
                                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -138,9 +145,10 @@
                     </form>
                 </div>
             </div>
-</div>
-           
+        </div>     
         </div>
     </div>
 </section>
 
+</body>
+</html>
