3fed4b06ad5bd196d2cdf116a16216dd522ba72d
unknown <nilesh.netdeveloper@gmail.com>
2024-08-02 15:11:32 +0530

    login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,54 +1,54 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-// use Illuminate\Contracts\Session\Session;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
-class AdminController extends Controller 
-{
-    public function adminindex(Request $request)
-    {
-        if($request->user()->role_as == 'user') 
-        {
-            return view('partydashboard');
-        }
-        else
-        {
-            return view('dashboard');
-        }
-        // elseif($request->user()->role_as === 'user') 
-        // return view('login');
-    }
-public function loginform()
-{
-    return view('login');
-}
-    public function adminlogin(Request $request) 
-    {
+// // use Illuminate\Contracts\Session\Session;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Session;
+// class AdminController extends Controller 
+// {
+//     public function adminindex(Request $request)
+//     {
+//         if($request->user()->role_as == 'user') 
+//         {
+//             return view('partydashboard');
+//         }
+//         else
+//         {
+//             return view('dashboard');
+//         }
+//         // elseif($request->user()->role_as === 'user') 
+//         // return view('login');
+//     }
+// public function loginform()
+// {
+//     return view('login');
+// }
+//     public function adminlogin(Request $request) 
+//     {
      
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+//         $request->validate([
+//             'email' => 'required|email',
+//             'password' => 'required',
+//         ]);
     
-        $credentials = $request->only('email', 'password');
+//         $credentials = $request->only('email', 'password');
     
-        if (Auth::guard('admin')->attempt($credentials)) {
-            return redirect()->intended(route('index')); 
-        }
+//         if (Auth::guard('admin')->attempt($credentials)) {
+//             return redirect()->intended(route('index')); 
+//         }
     
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
-    }
-    public function logout(Request $request)
-    {
-        Auth::guard('admin')->logout();
-        return redirect('/admin/login');
-    }
+//         return back()->withErrors([
+//             'email' => 'The provided credentials do not match our records.',
+//         ]);
+//     }
+//     // public function logout(Request $request)
+//     // {
+//     //     Auth::guard('admin')->logout();
+//     //     return redirect('/admin/login');
+//     // }
 
-}
+// }
