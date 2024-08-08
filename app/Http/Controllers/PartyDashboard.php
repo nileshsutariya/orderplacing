b5ae944a15321ed97c2a30aefb4fe222ca63d8d3
nileshsutariya <85292697+nileshsutariya@users.noreply.github.com>
2024-08-08 11:49:25 +0530

    Update PartyDashboard.php

--- app/Http/Controllers/PartyDashboard.php
+++ app/Http/Controllers/PartyDashboard.php
@@ -1,12 +1,1 @@
-<?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class PartyDashboard extends Controller
-{
-    public function index(){
-        return view('partydashboard');
-    }
-}
